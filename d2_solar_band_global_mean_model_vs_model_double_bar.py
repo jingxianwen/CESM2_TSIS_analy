f73ed462a78038b87786d631ded2c53611e93ee0
jingxianwen <jingxianwen@live.cn>
2020-05-03 21:47:09 -0400

    new bar plots

--- d2_solar_band_global_mean_model_vs_model_double_bar.py
+++ d2_solar_band_global_mean_model_vs_model_double_bar.py
@@ -33,18 +33,20 @@ from pylab import *
 # data path
 ctl_name="CTL" #os.environ["ctl_name"]
 exp_name="TSIS" #os.environ["exp_name"]
-#ctl_pref="solar_CTL_cesm211_VIS_icealb_ETEST-f19_g17-ens_mean_2010-2019"
-#exp_pref="solar_TSIS_cesm211_VIS_icealb_ETEST-f19_g17-ens_mean_2010-2019"
-ctl_pref="solar_CTL_cesm211_ETEST-f19_g17-ens0_fssd"
-exp_pref="solar_TSIS_cesm211_ETEST-f19_g17-ens0_fssd"
+ctl_pref="solar_CTL_cesm211_VIS_icealb_ETEST-f19_g17-ens_mean_2010-2019"
+exp_pref="solar_TSIS_cesm211_VIS_icealb_ETEST-f19_g17-ens_mean_2010-2019"
+ctl_fssd_pref="solar_CTL_cesm211_ETEST-f19_g17-ens0_fssd"
+exp_fssd_pref="solar_TSIS_cesm211_ETEST-f19_g17-ens0_fssd"
 
 fpath_ctl="/raid00/xianwen/cesm211_solar/"+ctl_pref+"/climo/"
 fpath_exp="/raid00/xianwen/cesm211_solar/"+exp_pref+"/climo/"
+fpath_ctl_fssd="/raid00/xianwen/cesm211_solar/"+ctl_fssd_pref+"/climo/"
+fpath_exp_fssd="/raid00/xianwen/cesm211_solar/"+exp_fssd_pref+"/climo/"
  
-years=np.arange(2000,2001) 
+years=np.arange(2010,2020) 
 months_all=["01","02","03","04","05","06","07","08","09","10","11","12"]
 
-var_group_todo=4
+var_group_todo=22
 # variable group 1:
 if var_group_todo==1:
    varnms=np.array(["FSSU13","FSSU12","FSSU11","FSSU10","FSSU09",\
@@ -52,19 +54,26 @@ if var_group_todo==1:
            "FSSU03","FSSU02","FSSU01","FSSU14"])
    var_long_name="Band-by-Band TOA Upward SW"
    figure_name="Band_by_Band_TOA_Upward_SW_ANN_VIS_icealb"
-   units=r"Wm$^-^2$"
+   units=r"Wm$^-$$^2$"
 
 # variable group 2:
-if var_group_todo==2:
-   varnms=np.array(["FSSUS13","FSSUS12","FSSUS11","FSSUS10","FSSUS09",\
+if var_group_todo==22:
+
+   varnms_sub_toa=np.array(["FSSU13","FSSU12","FSSU11","FSSU10","FSSU09",\
+           "FSSU08","FSSU07","FSSU06","FSSU05","FSSU04",\
+           "FSSU03","FSSU02","FSSU01","FSSU14"])
+   varnms_toa=np.array(["FSSD13","FSSD12","FSSD11","FSSD10","FSSD09",\
+           "FSSD08","FSSD07","FSSD06","FSSD05","FSSD04",\
+           "FSSD03","FSSD02","FSSD01","FSSD14"])
+   varnms_sub_sfc=np.array(["FSSUS13","FSSUS12","FSSUS11","FSSUS10","FSSUS09",\
            "FSSUS08","FSSUS07","FSSUS06","FSSUS05","FSSUS04",\
            "FSSUS03","FSSUS02","FSSUS01","FSSUS14"])
-   varnms_sub=np.array(["FSSDS13","FSSDS12","FSSDS11","FSSDS10","FSSDS09",\
+   varnms_sfc=np.array(["FSSDS13","FSSDS12","FSSDS11","FSSDS10","FSSDS09",\
            "FSSDS08","FSSDS07","FSSDS06","FSSDS05","FSSDS04",\
            "FSSDS03","FSSDS02","FSSDS01","FSSDS14"])
    var_long_name="Band-by-Band Surface net Upward SW"
-   figure_name="Band_by_Band_surface_net_Upward_SW_ANN_VIS_icealb"
-   units=r"Wm$^-^2$"
+   figure_name="Band_by_Band_net_absorb_TOA&SFC_SW_ANN"
+   units=r"Wm$^-$$^2$"
 
 # variable group 3 (clear sky):
 if var_group_todo==3:
@@ -76,7 +85,7 @@ if var_group_todo==3:
            "FSSUCLR03","FSSUCLR02","FSSUCLR01","FSSUCLR14"])
    var_long_name="Band-by-Band TOA SWCF"
    figure_name="Band_by_Band_TOA_SWCF_ANN_VIS_icealb"
-   units=r"Wm$^-^2$"
+   units=r"Wm$^-$$^2$"
 
 # variable group 4:
 if var_group_todo==4:
@@ -90,19 +99,31 @@ if var_group_todo==4:
 #f1=fpath_ctl+"solar_TSIS_cesm211_standard-ETEST-f19_g17-ens1.cam.h0.0001-01.nc"
 #f2=fpath_exp+"tsis_ctl_cesm211_standard-ETEST-f19_g17-ens1.cam.h0.0001-01.nc"
 
-means_yby_ctl=np.zeros((years.size,varnms.size)) #year by year mean for each variable
-means_yby_exp=np.zeros((years.size,varnms.size)) #year by year mean for each variable
-means_ctl=np.zeros((varnms.size)) #multi-year mean for each variable
-means_exp=np.zeros((varnms.size)) #multi-year mean for each variable
-diffs=np.zeros((varnms.size)) #multi-year exp-ctl diff for each variable
-pvals=np.zeros((varnms.size)) #pvalues of ttest
+means_yby_ctl_toa=np.zeros((years.size,varnms_toa.size)) #year by year mean for each variable
+means_yby_exp_toa=np.zeros((years.size,varnms_toa.size)) #year by year mean for each variable
+means_ctl_toa=np.zeros((varnms_toa.size)) #multi-year mean for each variable
+means_exp_toa=np.zeros((varnms_toa.size)) #multi-year mean for each variable
+diffs_toa=np.zeros((varnms_toa.size)) #multi-year exp-ctl diff for each variable
+pvals_toa=np.zeros((varnms_toa.size)) #pvalues of ttest
+
+means_yby_ctl_sfc=np.zeros((years.size,varnms_sfc.size)) #year by year mean for each variable
+means_yby_exp_sfc=np.zeros((years.size,varnms_sfc.size)) #year by year mean for each variable
+means_ctl_sfc=np.zeros((varnms_sfc.size)) #multi-year mean for each variable
+means_exp_sfc=np.zeros((varnms_sfc.size)) #multi-year mean for each variable
+diffs_sfc=np.zeros((varnms_sfc.size)) #multi-year exp-ctl diff for each variable
+pvals_sfc=np.zeros((varnms_sfc.size)) #pvalues of ttest
 
 for iy in range(0,years.size): 
     # open data file
     fctl=fpath_ctl+ctl_pref+"_ANN_"+str(years[iy])+".nc"
     fexp=fpath_exp+exp_pref+"_ANN_"+str(years[iy])+".nc"
+    fctl_fssd=fpath_ctl_fssd+ctl_fssd_pref+"_ANN_"+"2000.nc"
+    fexp_fssd=fpath_exp_fssd+exp_fssd_pref+"_ANN_"+"2000.nc"
+
     file_ctl=netcdf_dataset(fctl,"r")
     file_exp=netcdf_dataset(fexp,"r")
+    file_ctl_fssd=netcdf_dataset(fctl_fssd,"r")
+    file_exp_fssd=netcdf_dataset(fexp_fssd,"r")
     
     # read lat and lon
     lat=file_ctl.variables["lat"]
@@ -114,7 +135,7 @@ for iy in range(0,years.size):
     #stats_difp=np.zeros((14))
     #print(stats_ctl)
     # read data and calculate mean/min/max
-    for iv in range(0,varnms.size):
+    for iv in range(0,varnms_toa.size):
         if var_group_todo is 1:
            dtctl=file_ctl.variables[varnms[iv]]
            dtexp=file_exp.variables[varnms[iv]] 
@@ -127,57 +148,81 @@ for iy in range(0,years.size):
         elif var_group_todo is 4:
            dtctl=file_ctl.variables[varnms[iv]]
            dtexp=file_exp.variables[varnms[iv]] 
+        elif var_group_todo is 22:
+           dtctl_toa=file_ctl_fssd.variables[varnms_toa[iv]][:,:,:]-file_ctl.variables[varnms_sub_toa[iv]][:,:,:]
+           dtexp_toa=file_exp_fssd.variables[varnms_toa[iv]][:,:,:]-file_exp.variables[varnms_sub_toa[iv]][:,:,:]
+           dtctl_sfc=file_ctl.variables[varnms_sfc[iv]][:,:,:]-file_ctl.variables[varnms_sub_sfc[iv]][:,:,:]
+           dtexp_sfc=file_exp.variables[varnms_sfc[iv]][:,:,:]-file_exp.variables[varnms_sub_sfc[iv]][:,:,:]
         #dtdif=dtexp[:,:,:]-dtctl[:,:,:]
-        means_yby_ctl[iy,iv]=get_area_mean_min_max(dtctl[:,:,:],lat[:])[0]
-        means_yby_exp[iy,iv]=get_area_mean_min_max(dtexp[:,:,:],lat[:])[0]
+        means_yby_ctl_toa[iy,iv]=get_area_mean_min_max(dtctl_toa[:,:,:],lat[:])[0]
+        means_yby_exp_toa[iy,iv]=get_area_mean_min_max(dtexp_toa[:,:,:],lat[:])[0]
+        means_yby_ctl_sfc[iy,iv]=get_area_mean_min_max(dtctl_sfc[:,:,:],lat[:])[0]
+        means_yby_exp_sfc[iy,iv]=get_area_mean_min_max(dtexp_sfc[:,:,:],lat[:])[0]
         #stats_dif[i]=get_area_mean_min_max(dtdif[:,:,:],lat[:])[0]
         #stats_difp[i]=stats_dif[0]/stats_ctl[0]*100.
 
 # compute multi-year mean and ttest
-means_ctl=np.mean(means_yby_ctl,axis=0)
-means_exp=np.mean(means_yby_exp,axis=0)
-diffs=means_exp-means_ctl
-#ttest=stats.ttest_ind(means_yby_ctl,means_yby_exp,axis=0)
-pvalues=0. # ttest.pvalue
-print(pvalues)
+means_ctl_toa=np.mean(means_yby_ctl_toa,axis=0)
+means_exp_toa=np.mean(means_yby_exp_toa,axis=0)
+diffs_toa=means_exp_toa-means_ctl_toa
+ttest_toa=stats.ttest_ind(means_yby_ctl_toa,means_yby_exp_toa,axis=0)
+pvalues_toa= ttest_toa.pvalue
+
+means_ctl_sfc=np.mean(means_yby_ctl_sfc,axis=0)
+means_exp_sfc=np.mean(means_yby_exp_sfc,axis=0)
+diffs_sfc=means_exp_sfc-means_ctl_sfc
+ttest_sfc=stats.ttest_ind(means_yby_ctl_sfc,means_yby_exp_sfc,axis=0)
+pvalues_sfc= ttest_sfc.pvalue
+
 siglev=0.05
-diffs_sig=np.zeros(diffs.size)
-diffs_unsig=np.zeros(diffs.size)
-diffs_sig=diffs
-#for ip in range(pvalues.size):
-#    if pvalues[ip] < siglev:
-#        diffs_sig[ip]=diffs[ip]
-#    else:
-#        diffs_unsig[ip]=diffs[ip]
+diffs_sig_toa=np.zeros(diffs_toa.size)
+diffs_unsig_toa=np.zeros(diffs_toa.size)
+diffs_sig_sfc=np.zeros(diffs_toa.size)
+diffs_unsig_sfc=np.zeros(diffs_toa.size)
+
+for ip in range(pvalues_toa.size):
+    if pvalues_toa[ip] < siglev:
+        diffs_sig_toa[ip]=diffs_toa[ip]
+    else:
+        diffs_unsig_toa[ip]=diffs_toa[ip]
+
+for ip in range(pvalues_sfc.size):
+    if pvalues_sfc[ip] < siglev:
+        diffs_sig_sfc[ip]=diffs_sfc[ip]
+    else:
+        diffs_unsig_sfc[ip]=diffs_sfc[ip]
 
 
 # make the plot
 fig=plt.figure(figsize=(7,8.5))
 ax1=fig.add_axes([0.13,0.62,0.78,0.33])
 #ax2=fig.add_axes([0.13,0.14,0.78,0.33])
-x=[0,1,2,3,4,5,6,7,8,9,10,11,12,13]
+x=np.array([0,1,2,3,4,5,6,7,8,9,10,11,12,13])
+#x=[0.5,1.0,1.5,2.0,2.5,3.,3.5,4.,4.5,5.,5.5,6.,6.5,7.]
 bands=["0.2-0.26","0.26-0.34","0.34-0.44","0.44-0.63","0.63-0.78","0.78-1.24","1.24-1.3","1.3-1.63","1.63-1.94","1.94-2.15","2.15-2.5","2.5-3.08","3.08-3.85","3.85-12.2"]
-
-ax1.bar(bands,means_ctl,color="indigo") #"tab:blue"
+ax1.bar(x-0.2,means_ctl_toa,width=0.5,color="indigo",label="TOA") #"tab:blue"
+ax1.bar(x+0.2,means_ctl_sfc,width=0.5,color="limegreen",label="Surface") #"tab:blue"
 #ax1.set_title(var_long_name+" (CESM2)",fontsize=14)
-ax1.set_title("Band-Integrated SSI" +" (CESM2)",fontsize=14)
+ax1.set_title("Net Flux at TOA&Surface" +" (CESM2)",fontsize=14)
 ax1.set_ylabel(units,fontsize=12)
 #ax1.set_xlabel("Band wave length",fontsize=12)
 ax1.grid(True)
 ax1.set_axisbelow(True)
 ax1.xaxis.grid(color='lightgray', linestyle=':')
 ax1.yaxis.grid(color='lightgray', linestyle=':')
-#plt.xticks(x,bands,rotation=-45)
-ax1.set_xticklabels(labels=bands,rotation=-45,fontsize=12)
-#ylocs,ylabels=yticks()
-#print(ylocs,ylabels)
-#ax1.set_yticklabels(labels=ylabels,fontsize=12)
+plt.xticks(x,bands,rotation=-45,fontsize=12)
+#ax1.set_xticklabels(labels=bands,rotation=-45,fontsize=12)
 plt.yticks(fontsize=12)
+ax1.legend(fontsize=14)
 
 ax2=fig.add_axes([0.13,0.14,0.78,0.33])
 #bars=[None]*diffs_sig.size
-ax2.bar(bands,diffs_sig,color="indigo",hatch="//",edgecolor="white")
-ax2.bar(bands,diffs_unsig,color="indigo")
+#ax2.bar(bands,diffs_sig_toa,color="indigo",hatch="//",edgecolor="white")
+#ax2.bar(bands,diffs_unsig_toa,color="indigo")
+ax2.bar(x-0.2,diffs_sig_toa,width=0.5,color="indigo",hatch="//",edgecolor="white")
+ax2.bar(x-0.2,diffs_unsig_toa,width=0.5,color="indigo")
+ax2.bar(x+0.2,diffs_sig_sfc,width=0.5,color="limegreen",hatch="//",edgecolor="white")
+ax2.bar(x+0.2,diffs_unsig_sfc,width=0.5,color="limegreen")
 
 #ax2.set_title("Diff in "+var_long_name+" (TSIS-1 - CESM2)",fontsize=14)
 ax2.set_title("Differences"+" (TSIS-1 - CESM2)",fontsize=14)
@@ -185,10 +230,11 @@ ax2.set_ylabel(units,fontsize=14)
 ax2.set_xlabel("Band wave length",fontsize=14)
 ax2.grid(True)
 ax2.set_axisbelow(True)
-ax2.set_ylim(-1.25,1.25)
+#ax2.set_ylim(-1.05,0.8)
 ax2.xaxis.grid(color='lightgray', linestyle=':')
 ax2.yaxis.grid(color='lightgray', linestyle=':')
-ax2.set_xticklabels(labels=bands,rotation=-45,fontsize=12)
+#ax2.set_xticklabels(labels=bands,rotation=-45,fontsize=12)
+plt.xticks(x,bands,rotation=-45,fontsize=12)
 plt.yticks(fontsize=12)
 plt.savefig(figure_name+".eps")
 plt.show()
